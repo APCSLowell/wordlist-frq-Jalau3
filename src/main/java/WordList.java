70c187d69032aa315dc12c261ed582518b6df569
Jalau3 <143014960+Jalau3@users.noreply.github.com>
2024-04-03 09:30:22 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,19 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
-    
+    int count = 0;
+    for(String word:myList){
+      if(word.length()==len)
+        count++;
+    }
+    return count;
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
-
+    for(String word:myList){
+      if(word.length()==len)
+        myList.remove(word);
+    }
   }
 }
